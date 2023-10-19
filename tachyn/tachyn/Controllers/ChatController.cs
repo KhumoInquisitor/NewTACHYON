0a93aca6ec14c7542ea13ca3551493710f990fae
KhumoInquisitor <s224257587@Mandela.ac.za>
2023-10-19 14:31:08 +0200

    kk14:00

---
+++ tachyn/tachyn/Controllers/ChatController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tachyon.Areas.Identity.Data;
+using Tachyon.Models;
+
+namespace Tachyon.Controllers
+{
+	public class ChatController : Controller
+	{
+        private readonly TachyonDbContext _db;
+        public ChatController(TachyonDbContext db)
+        {
+            _db = db;
+        }
+        // This could use a database or in-memory list for demo purposes
+        private static List<Message> _messages = new List<Message>();
+
+		[HttpPost]
+		public ActionResult SendMessage(string text)
+		{
+			// Add the message to the database or in-memory list
+			_messages.Add(new Message
+			{
+				UserName = User.Identity.Name,
+				Text = text,
+				Timestamp = DateTime.Now
+			});
+
+			return Json(new { success = true });
+		}
+
+		public ActionResult GetMessages()
+		{
+			return View(_messages);
+		}
+	}
+
+}
---
+++ tachyn/tachyn/Controllers/ProcedureController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tachyon.Areas.Identity.Data;
+using Tachyon.Models;
+
+namespace Tachyon.Controllers
+{
+	public class ProcedureController : Controller
+	{
+		private readonly TachyonDbContext _db;
+		public ProcedureController(TachyonDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Land()
+		{
+			return View();
+		}
+		public IActionResult JointInjection()
+		{
+			return View();
+		}
+		public IActionResult Lung()
+		{
+			return View();
+		}
+		public IActionResult MinorSurgery()
+		{
+			return View();
+		}
+
+		public IActionResult ProcedureList()
+		{
+			IEnumerable<Procedure> proList = _db.Procedure;
+			return View(proList);
+		}
+		public IActionResult CreateProcedure()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult CreateProcedure(Procedure Procedure)
+		{
+			if (ModelState.IsValid)
+			{
+                _db.Procedure.Add(Procedure);
+                _db.SaveChanges();
+				return RedirectToAction("ProcedureList");
+			}
+			return View(Procedure);
+		}
+		public IActionResult updateProcedure(int? ID)
+		{
+			if (ID == null || ID == 0)
+			{
+				return NotFound();
+			}
+			var proList = _db.Procedure.Find(ID);
+			if (proList == null)
+			{
+				return NotFound();
+			}
+			return View(proList);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult updateProcedure(Procedure Procedure)
+		{
+            _db.Procedure.Update(Procedure);
+            _db.SaveChanges();
+			return RedirectToAction("ProcedureList");
+		}
+
+		public IActionResult DeleteProcedure(int? ID)
+		{
+			var proList = _db.Procedure.Find(ID);
+			if (proList == null)
+			{
+				return NotFound();
+			}
+            _db.Procedure.Remove(proList);
+            _db.SaveChanges();
+			return RedirectToAction("ProcedureList");
+
+		}
+	}
+}
